7518ac206f250a5d359ac315220e64550c7f3d40
lexBogos <lex.bogos@gmail.com>
2018-09-21 07:13:36 +0300

    solution_quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+var my_string = equation
+var a = Number(my_string.substring(0, my_string.indexOf('x')-3));
+
+my_string = my_string.substring([my_string.indexOf('x')+4,])
+
+if (my_string[0] == '+')
+{
+my_string = my_string.substring([2,])
+var b = Number(my_string.substring(0, my_string.indexOf('x')-3));
 }
+
+else
+{
+	my_string = my_string.substring([2,])
+	var b = -1*Number(my_string.substring(0, my_string.indexOf('x')-3));
+}
+
+my_string = my_string.substring([my_string.indexOf('x')+2,])
+
+if (my_string[0] == '+')
+{
+var c = Number(my_string.substring(2, ));
+}
+
+else
+{
+	var c  = -1*Number(my_string.substring(2, ));
+}
+
+
+x1=Math.round((-b-Math.pow(b*b-4*a*c,0.5))/(2*a))
+x2=Math.round((-b+Math.pow(b*b-4*a*c,0.5))/(2*a))
+
+
+return [x1,x2].sort(function(a,b){
+        return a-b;
+      })
+}
+
+
+    
